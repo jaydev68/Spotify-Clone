378b4fa3bd480024ee220dd5874fc095f6fb87ae
jaydev68 <jaydevrathva54@gmail.com>
2026-04-06 03:59:42 +0530

    web host

--- script.js
+++ script.js
@@ -1,55 +1,3 @@
-// console.log('Lets write JavaScript')
-
-// async function getSongs() {
-
-//     let a = await fetch("http://127.0.0.1:3000/songs/")
-//     let response = await a.text();
-//     console.log(response)
-//     let div = document.createElement("div")
-//     div.innerHTML = response;
-//     let as = div.getElementsByTagName("a")
-//     let songs = []
-//     for (let index = 0; index < as.length; index++) {
-//         const element = as[index];
-//         if (element.href.endsWith(".mp3")) {
-//             songs.push(element)
-//         }
-//     }
-//     return songs
-// }
-
-// async function main() {
-//     //Get the list of all the songs
-//     let songs = await getSongs()
-//     console.log(songs)
-
-//    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
-//    for (const song of songs ){
-//     songUL.innerHTML = songUL.innerHTML + `<li> ${song} </li> `;
-//    }
-
-
-//     //Play songs
-//     var audio = new Audio(songs[0]);
-// // audio.play();
-// }
-// main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 console.log('Lets write JavaScript')
 let currentSong = new Audio();
@@ -69,7 +17,7 @@ function secondsToMinutesSeconds(seconds) {
 }
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/`)
+    let a = await fetch(`/songs/${folder}/`)
     let response = await a.text();
     console.log(response)
 
